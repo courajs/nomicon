482426674b7a694af516c5163eb516f6807dd54e
Aaron Sikes <aaron@sikes.io>
2019-02-20 19:47:41 -0500

    Fix updating an existing page

--- app/controllers/page.js
+++ app/controllers/page.js
@@ -4,9 +4,9 @@ import {inject} from '@ember/service';
 export default Controller.extend({
   data: inject(),
   async save(page) {
-    page.id = this.model.id;
+    page.id = this.model.page.id;
     await this.data.updatePage(page);
-    let model = await this.data.getPage(this.model.id);
+    let model = await this.data.getPage(this.model.page.id);
     this.set('model', model);
-  }
+  },
 });
