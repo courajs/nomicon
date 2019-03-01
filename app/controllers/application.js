a3abb00bc551b156aa8ae9fbab471628da43f0d9
Aaron Sikes <aaron@sikes.io>
2019-03-01 14:02:04 -0500

    Show dev/prod signifier by build environment

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,10 +2,12 @@ import Controller from '@ember/controller';
 import {inject} from '@ember/service';
 
 import {bound} from 'nomicon/lib/hotkeys';
+import env from 'nomicon/config/environment';
 
 export default Controller.extend({
   data: inject(),
 
+  environment: env.environment,
   showModal: false,
 
   hotkeys: bound({
