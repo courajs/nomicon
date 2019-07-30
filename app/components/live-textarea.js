9337feba8e2308cb4e63a2446577e03a92769605
Aaron Sikes <aaron@sikes.io>
2019-07-30 14:53:58 -0400

    Extract live text areas into a component

--- app/components/live-textarea.js
+++ app/components/live-textarea.js
@@ -1,22 +1,24 @@
-import Controller from '@ember/controller';
+import Component from '@glimmer/component';
 import {tracked} from '@glimmer/tracking';
 import {inject as service} from '@ember/service';
 
 import LiveSequence from 'nomicon/lib/live/sequence';
 
-import * as rxjs from 'rxjs';
-
-window.rxjs = rxjs;
-
-export default class extends Controller {
+export default class extends Component {
   @service auth;
   @service sync;
   
-  @tracked collection;
+  @tracked collection
   @tracked sequence;
 
+  constructor() {
+    super(...arguments);
+    this.init();
+  }
+
   async init() {
-    this.collection = await this.sync.liveCollection('test4');
+    console.log('init');
+    this.collection = await this.sync.liveCollection(this.args.collection);
     await this.auth.awaitAuth;
     this.sequence = new LiveSequence(this.collection, this.auth.clientId);
   }
